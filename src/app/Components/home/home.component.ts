ea15219a7d761a896385a72bc7386928180185dd
Artback <artback@openmailbox.org>
2019-11-10 12:51:34 +0100

    Add recipe graphql

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,24 +1,35 @@
 import {Component, OnInit} from '@angular/core';
 import {debounceTime, distinctUntilChanged, filter,  switchMap} from 'rxjs/internal/operators';
 import {FormControl} from '@angular/forms';
-
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 @Component({
   selector: 'app-home',
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
-
 export class HomeComponent implements  OnInit {
   recipes: any[];
   ratings: any[];
   private search: FormControl;
+
+  constructor(private apollo: Apollo) {}
   ngOnInit() {
     this.search = new FormControl();
     this.search.valueChanges.pipe(
       debounceTime(400),
       filter(preventEmpty => preventEmpty),
       distinctUntilChanged(),
+      switchMap((query: String) => this.apollo.query(gql`query {
+        recipes(query: ${query},begin: 0,end: 10){
+            uri
+            calories
+            cautions
+            image
+        }
+      }`).toPromise().then(data => data.data))
     ).subscribe(recipes => {
+      this.recipes = recipes as any[];
     });
   }
   onRatingChange(event: {rating: number, uri: string}) {
